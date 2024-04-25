3706664ad0f16fe4ba34f6bc5552396ae1b69604
Dmytro Fedoriaka <fedimser@gmail.com>
2024-04-25 00:12:16 -0700

    optimize Dijkstra

--- Dijkstra.cpp
+++ Dijkstra.cpp
@@ -1,61 +1,70 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int64_t INF = 1000000000;
+const int64_t INF = 1000000000000000000;  // 10^18.
 struct Graph {
   int n;
   vector<vector<pair<int, int64_t>>> adj;
-  vector<int64_t> d; // d[i] = min dist(s, i).
-  vector<int> p; // prev vertex in path.
-  
-  Graph(int n_) {
-  	n = n_;
-  	adj.resize(n);
-  }
-  
-  void addEdge(int x, int y, int64_t w) {
-    assert(x>=0 && x<n && y>=0 && y<n && w<INF);
+  vector<int64_t> d;  // d[i] = min dist(s, i).
+  vector<int> p;      // prev vertex in path.
+
+  Graph(int n_) : n(n_), adj(n_){};
+
+  void AddEdge(int x, int y, int64_t w) {
+    assert(0 <= x && x < n);
+    assert(0 <= y && y < n);
+    assert(0 <= w && w < INF);
     adj[x].push_back({y, w});
     adj[y].push_back({x, w});
   }
 
   // s - start vertex. Complexity O (m*log(n)).
-  void dijkstra(int s) {
+  void Dijkstra(int s) {
     d.assign(n, INF);
     p.assign(n, -1);
-	d[s] = 0;
-	set<pair<int64_t, int>> q;
-	q.insert({0, s});
-	while (!q.empty()) {
-	  int v = q.begin()->second;
-	  q.erase(q.begin());
-	  for (auto edge : adj[v]) {
-	    int to = edge.first;
-	    int len = edge.second;
-        if (d[v] + len < d[to]) {
-	      q.erase({d[to], to});
-	      d[to] = d[v] + len; 
-	      p[to] = v;
-	      q.insert({d[to], to});
-	    }
-	  }
+    d[s] = 0;
+    priority_queue<pair<int64_t, int>> q;
+    q.push({0, s});
+    while (!q.empty()) {
+      int64_t sd = -q.top().first;
+      int v = q.top().second;
+      q.pop();
+      if (sd != d[v]) {
+        continue;
+      }
+      for (const auto& edge : adj[v]) {
+        int to = edge.first;
+        int64_t new_d = d[v] + edge.second;
+        if (new_d < d[to]) {
+          d[to] = new_d;
+          p[to] = v;
+          q.push({-d[to], to});
+        }
+      }
     }
   }
 };
 
 int main() {
-    Graph g(5);
-    g.addEdge(0, 1, 1);
-    g.addEdge(1, 3, 2);
-    g.addEdge(0, 2, 4);
-    g.addEdge(2, 3, 8);
-    g.addEdge(0, 3, 16);
-    g.addEdge(3, 4, 32);
+  Graph g(5);
+  g.AddEdge(0, 1, 1);
+  g.AddEdge(1, 3, 2);
+  g.AddEdge(0, 2, 4);
+  g.AddEdge(2, 3, 8);
+  g.AddEdge(0, 3, 16);
+  g.AddEdge(3, 4, 32);
 
-    g.dijkstra(0);
-    assert(g.d == (vector<int64_t> {0, 1, 4, 3, 35}));
-    assert(g.p == (vector<int> {-1, 0, 0, 1, 3}));
-    g.dijkstra(3);
-    assert(g.d == (vector<int64_t> {3, 2, 7, 0, 32}));
-    assert(g.p == (vector<int> {1, 3, 0, -1, 3}));
+  g.Dijkstra(0);
+  assert(g.d == (vector<int64_t>{0, 1, 4, 3, 35}));
+  assert(g.p == (vector<int>{-1, 0, 0, 1, 3}));
+  g.Dijkstra(3);
+  assert(g.d == (vector<int64_t>{3, 2, 7, 0, 32}));
+  assert(g.p == (vector<int>{1, 3, 0, -1, 3}));
+
+  Graph g2(3);
+  g2.AddEdge(0, 1, INF - 5);
+  g2.AddEdge(1, 2, 3);
+  g2.Dijkstra(0);
+  assert(g2.d == (vector<int64_t>{0, INF - 5, INF - 2}));
+  assert(g2.p == (vector<int>{-1, 0, 1}));
 }
--- debug.h
+++ debug.h
@@ -1,8 +1,8 @@
 // Debugging utils for competitive programming. (c) Dmytro Fedoriaka, 2024.
 // This is intended to be included as header in local-only mode.
 #include <iostream>
-#include <vector>
 #include <map>
+#include <vector>
 
 template <typename T>
 void DebugPrint(T &&v) {
@@ -14,32 +14,51 @@ void DebugPrint(std::vector<T> &v) {
   bool c = false;
   std::cout << "[";
   for (T &x : v) {
-    if (c) {std::cout << ",";} else {c=true;}
+    if (c) {
+      std::cout << ",";
+    } else {
+      c = true;
+    }
     std::cout << x;
   }
   std::cout << "]";
 }
 
 template <typename T1, typename T2>
-void DebugPrint(std::map<T1, T2> &m){
+void DebugPrint(std::map<T1, T2> &m) {
   bool c = false;
   std::cout << "{";
-  for (const auto &x : m)  {
-    if (c) {std::cout << ",";} else {c=true;}
+  for (const auto &x : m) {
+    if (c) {
+      std::cout << ",";
+    } else {
+      c = true;
+    }
     std::cout << x.first << ":" << x.second;
   }
   std::cout << "}";
 }
 
 template <typename T>
-void LOG(T &&v){
+void LOG(T &&v) {
   DebugPrint(v);
   std::cout << std::endl;
 }
 
 template <typename T, typename... Ts>
-void LOG(T &&v, Ts &&...r){
+void LOG(T &&v, Ts &&...r) {
   DebugPrint(v);
   std::cout << " ";
   LOG(std::forward<Ts>(r)...);
+}
+
+std::chrono::time_point<std::chrono::system_clock> __start_time =
+    std::chrono::system_clock::now();
+
+void ResetTime() { __start_time = std::chrono::system_clock::now(); }
+
+void LogTime(std::string message) {
+  auto diff = std::chrono::system_clock::now() - __start_time;
+  auto diff_ms = std::chrono::duration_cast<std::chrono::milliseconds>(diff);
+  std::cout << message << " " << diff_ms.count() << "ms" << std::endl;
 }
